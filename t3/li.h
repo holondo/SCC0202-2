3dea5d9ff2a99072bd241ac5df1a074b00c6c363
Igor Martins <cardozo@usp.br>
2019-11-20 11:11:56 -0300

    Merging all lists and modifyng to use data type specified in colecao.h

---
+++ t3/li.h
@@ -0,0 +1,13 @@
+#ifndef lo_h
+    #define lo_h
+
+    #include "colecao.h"
+    
+    void inserir_LO(No *ini, No *n); 
+    void inserir_LP(No *ini, No *n); 
+    void inserir_LU(No *ini, No *n); 
+    int buscar_LI(No *ini, elem x);
+    void imprimir_LI(No *ini);
+    void destruir_LI(No *ini;
+
+#endif
